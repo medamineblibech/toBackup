b59dfe54c436b86ba22fc5b060fd6edf3a7e1ace
med363 <blibechmedamine@gmail.com>
2022-08-01 00:09:41 -0700

    with livereload

--- back/server.js
+++ back/server.js
@@ -1,30 +1,28 @@
 const fs = require('fs')
 const express = require('express');
 const cors = require('cors')
-/*const livereload = require('livereload');
+const livereload = require('livereload');
 const connectLiveReload = require("connect-livereload");
-
-/*app.set('view engine','ejs')
-
-const path = require('path');*/
-
+const path = require('path');
 const app = express()
 app.use(cors())
 app.use(express.json())
-/*app.use(connectLiveReload())*/
+app.use(connectLiveReload())
 const PORT = 4000
-
-
-const dir = '../backups'
-
-/*const liveReloadServer = livereload.createServer();
-liveReloadServer.watch(path.join(__dirname,'back'))
-
+const dir = '../public/backups'
+const liveReloadServer = livereload.createServer();
+liveReloadServer.watch(path.join(__dirname,'public'))
 liveReloadServer.server.once('connection',()=>{
     setTimeout(()=>{
         liveReloadServer.refresh("/")
     },100)
-})*/
+})
+
+
+
+
+
+
 let files = fs.readdirSync(dir)
 
 
@@ -47,7 +45,6 @@ setInterval(()=>{
         if (err) {
             return res.status(400).json({ output: null, error: err.message })
         }
-
         res.status(200).json({ output: stdout, error: null })
     })
 })*/
