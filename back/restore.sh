8fe6bde3ad9ccdca8a33750a9acdc1d025bdb08d
houda921 <houdasayar12@gmail.com>
2022-08-17 11:48:16 +0100

    add the id of the database

--- back/restore.sh
+++ back/restore.sh
@@ -3,11 +3,33 @@
 # OPTIONS
 #----------------------------------------
 USER='root'       # MySQL User
-PASSWORD='Myp@ss123456789' # MySQL Password
-BACKUP_PATH='/home/sgbd/Desktop/backups'
+PASSWORD='houda' # MySQL Password
+DAYS_TO_KEEP=30    # 0 to keep forever
+GZIP=0            # 1 = Compress
+BACKUP_PATH='/home/houda/stage_i4tech/toBackup/backups'
 #----------------------------------------
 
-for db in $BACKUP_PATH/*.sql; do
-    echo "Restore database: $db "      
-    mysql -u $USER -p$PASSWORD  < $db
- done
+# Create the backup folder
+#if [ ! -d $BACKUP_PATH ]; then
+#  mkdir -p $BACKUP_PATH
+#fi
+echo 'hello 1 '
+# Get list of database names
+
+databases=`mysql -u $USER -p$PASSWORD -e "SHOW DATABASES;" | tr -d "|" | grep -v Database`
+echo 'hello 2' 
+echo $databases
+for db in $databases; 
+do
+  echo 'hello 3 '
+  if [ $db == 'information_schema' ] || [ $db == 'performance_schema' ] || [ $db == 'mysql' ] || [ $db == 'sys' ] || [ $db == 'phpmyadmin' ]; then
+    echo "Skipping database: $db"
+    continue
+  fi
+
+  date=$(date -I)
+  
+  echo "retoring up database: $db without compression"      
+  mysql -u $USER -p$PASSWORD  $db  < $BACKUP_PATH/$db-$date.sql
+  
+done
--- back/server.js
+++ back/server.js
@@ -7,16 +7,52 @@ const connectLiveReload = require("connect-livereload");
 /*app.set('view engine','ejs')
 
 const path = require('path');*/
+import mysql from 'mysql';
 
+
+///////////////
+
+
+
+// executes `pwd`
+/*child = exec("pwd", function (error, stdout, stderr) {
+    sys.print('stdout: ' + stdout);
+    sys.print('stderr: ' + stderr);
+    if (error !== null) {
+        console.log('exec error: ' + error);
+    }
+});*/
+
+
+// or more concisely
+var sys = require('sys')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec(' mysql -u $USER -p$PASSWORD  $db  < $BACKUP_PATH/$db-$date.sql', puts);
+
+
+
+
+
+///////////////
 const app = express()
 app.use(cors())
 app.use(express.json())
 /*app.use(connectLiveReload())*/
 const PORT = 4000
+const arrayt=[] 
+ myArray1=[]
+ myArray2=[]
+ myArray3=[]
 
 
-const dir = '../backups'
+var data1 = {}
+data1.table = []
+var  id=1 
 
+
+
+const dir = '/home/houda/stage_i4tech/toBackup/backups'
 /*const liveReloadServer = livereload.createServer();
 liveReloadServer.watch(path.join(__dirname,'back'))
 
@@ -26,18 +62,53 @@ liveReloadServer.server.once('connection',()=>{
     },100)
 })*/
 let files = fs.readdirSync(dir)
+table =[]
 
+console.log("this is the file ",files);
+console.log(files.length)
+//table = p.split(",")
 
-console.log(files);
+for(var i = 0; i < files.length; i++) {
+    myArray1 = files[i].split(".sql")
+
+    console.log( 'affichage de tableau '+myArray1)
+    for (j=0; j <myArray1.length-1 ; j++){
+
+        myArray2 = myArray1[j].split(/\s+/).join('')
+        console.log( "this is array 2",myArray2)
+    
+        myArray3= myArray2.split("-") 
+        console.log( myArray3)
+    
+        var obj = { } 
+        obj.id=id++
+        obj.name=myArray3[0],
+        obj.date=myArray3[1]+'-'+myArray3[2]+'-'+myArray3[3]
+        
+        
+    
+        data1.table.push(obj)
+    
+    } 
+}
+
 setInterval(()=>{
     app.get('/', (req, res) => {
 
-        let p = JSON.stringify(files);
+        let p = JSON.stringify( data1.table);
+        console.log(p)
+
+        
         res.send(p)
     }
     )
 },1000)
 
+
+
+app.get('/restore', (req, res) => {
+    cp.exec('mysql -u')
+})
 /*app.use(express.static(join(__dirname, '../src')))*/
 
 
