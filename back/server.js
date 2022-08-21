74bac251e1571a181f70473c55fda81da2e588fb
medamineblibech <110095953+medamineblibech@users.noreply.github.com>
2022-08-21 12:42:41 -0700

    Update server.js

--- back/server.js
+++ back/server.js
@@ -19,12 +19,34 @@ liveReloadServer.server.once('connection',()=>{
 })
 
 
-
-
-
+ myArray2=[]
+ myArray3=[]
+var data1 = {}
+data1.table = []
+var  id=1 
 const dir = '../public/backups'
 const files = fs.readdirSync(dir)
 console.log(files);
+table =[]
+console.log("this is the file ",files);
+console.log(files.length)
+//table = p.split(",")
+for(var i = 0; i < files.length; i++) {
+    myArray1 = files[i].split(".sql")
+    console.log( 'affichage de tableau '+myArray1)
+    for (j=0; j <myArray1.length-1 ; j++){
+        myArray2 = myArray1[j].split(/\s+/).join('')
+        console.log( "this is array 2",myArray2)
+        myArray3= myArray2.split("-") 
+        console.log( myArray3)
+        var obj = { } 
+        obj._id=id++
+        obj.files=myArray3[0]+'-'+myArray3[1]+'-'+myArray3[2]+'-'+myArray3[3]+'.sql'
+        data1.table.push(obj)
+    
+    } 
+}
+
 
 let connection = mysql.createConnection({
     host     : 'localhost',
@@ -37,7 +59,7 @@ let connection = mysql.createConnection({
 setInterval(()=>{
     app.get('/', (req, res) => {
 
-        let p = JSON.stringify(files);
+        let p = JSON.stringify(data1.table);
         res.send(p)
     }
     )
@@ -47,11 +69,21 @@ setInterval(()=>{
 
 
 
-app.get('/download', (req, res) => {
-    files.forEach((i)=>
-    res.download(i)
-    )
-})
+//nodejs api find data with id (pass id througn url)
+app.get('/download/:_id',(req, res) =>{
+
+    //   console.log(req);
+    //   console.log(req.params);               
+       const singleFile = data1.table.find((item) => item._id ===parseInt(req.params._id));
+       if(!singleFile){
+           return res.status(404).send('file not found')
+       }
+       console.log(singleFile);
+       let f=JSON.stringify(singleFile.files)
+       console.log(f);
+       res.send(f)
+   
+   })
 
 
 
