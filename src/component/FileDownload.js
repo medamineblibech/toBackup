c766a69ce09bef7970d444b8be5daa4e440f3bc1
med363 <blibechmedamine@gmail.com>
2022-08-26 06:53:30 -0700

    list files && download button

---
+++ src/component/FileDownload.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import axios from 'axios';
+import fileDownload from 'js-file-download';
+function FileDownload({ id, name }) {
+  const download = (e) => {
+    e.preventDefault()
+    axios({
+      url: `http://127.0.0.1:4000/download/${id}`,
+      method: "GET",
+      responseType: "blob"
+    }).then((res) => {
+      console.log(res);
+      fileDownload(res.data, `${name}`)
+    })
+  }
+  return (
+    <div>
+      <button className='buttonDownload' onClick={(e) => download(e)} >DOWNLOAD</button>
+    </div>
+  )
+}
+
+export default FileDownload
