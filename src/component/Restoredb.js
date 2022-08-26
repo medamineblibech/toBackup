80b6dcd4903d82f073a7feb06ba21999186d9c7d
medamineblibech <110095953+medamineblibech@users.noreply.github.com>
2022-08-26 07:39:03 -0700

    Update Restoredb.js

--- src/component/Restoredb.js
+++ src/component/Restoredb.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import axios from 'axios'
 
-const Restoredb = () => {
+const Restoredb = ({id}) => {
+  const restore = (e) => {
+    e.preventDefault()
+    axios({
+      url: `http://127.0.0.1:4000/restoredb/${id}`,
+      method: "GET",
+      responseType: "blob"
+    }).then((res) => {
+      console.log(res.data)
+    })
+  }
+
+
   return (
     <div>
-      <button className='buttonRestore'>RESTORE</button>
+      <button className='buttonRestore' onClick={(e)=>restore(e)}>RESTORE</button>
     </div>
   )
 }
